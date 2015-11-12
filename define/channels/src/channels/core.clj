ef90ed48e148e90f06ff6709c9647ccae726c8e3
Lutz Huehnken <lutz.huehnken@gmail.com>
2015-11-12 14:43:55 +0100

    channel experiments

--- define/channels/src/channels/core.clj
+++ define/channels/src/channels/core.clj
@@ -4,18 +4,18 @@
 
 (defn -main []
 
-(def responses-channel (chan))
+(def responses-channel (chan 10))
 
 (go 
   (println "========================")
-  (println (<! responses-channel))
+  (println (take 2 responses-channel))
   (println "========================"))
 
-(go (http/get "http://api.urbandictionary.com/v0/define?term=freak" {}
-          (fn [{:keys [status headers body error]}] (>! responses-channel body))))
+(defn async-get [url result]
+  (http/get url #(go (>! result (:body %)))))
 
-(go (http/get "https://en.wiktionary.org/w/api.php?format=xml&action=query&rvprop=content&prop=revisions&redirects=1&titles=freak" {}
-          (fn [{:keys [status headers body error]}] (>! responses-channel body))))
+(async-get "https://en.wiktionary.org/w/api.php?format=xml&action=query&rvprop=content&prop=revisions&redirects=1&titles=freak" responses-channel)
+(async-get "http://api.urbandictionary.com/v0/define?term=freak" responses-channel)
 
 ;; just so that it doesn't terminate before it puts out the text
 (Thread/sleep 2000))
